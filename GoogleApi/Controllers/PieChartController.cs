fa00e4a759db283ffea97e59870ef5d65d85eb80
Thanakrit Kaewsomsri <tice132547@gmauil.com>
2024-12-03 04:09:27 +0700

    first commit

---
+++ GoogleApi/Controllers/PieChartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoogleApi.Models;
+
+namespace GoogleApi.Controllers
+{
+    public class PieChartController : Controller
+    {
+        // GET: PieChart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetDataJson()
+        {
+            var db = new Database1Entities();
+            var data = db.Sales.Select(s => new { ProductName = s.ProductName, Sales = s.Sales }).ToList();
+
+            return Json(new { JSONList = data }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
